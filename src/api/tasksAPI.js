64571375819ba1b1cdcbc93823094efd1953f377
Daniil Boriskin <daniilboriskin21@yandex.ru>
2026-09-08 21:21:33 +0300

    feat: добавлен роутинг и страницы задач; интегрированы компоненты Router и RouterLink

--- src/api/tasksAPI.js
+++ src/api/tasksAPI.js
@@ -9,6 +9,15 @@ const tasksAPI = {
     return fetch(URL).then((response) => response.json());
   },
 
+  getById: (id) => {
+    return fetch(`${URL}/${id}`).then((response) => {
+      if (!response.ok) {
+        throw new Error(`Task not found`);
+      }
+      return response.json();
+    });
+  },
+
   add: (task) => {
     return fetch(URL, {
       method: "POST",
@@ -18,7 +27,7 @@ const tasksAPI = {
   },
 
   delete: (id) => {
-    return fetch(`${URL}${id}`, {
+    return fetch(`${URL}/${id}`, {
       method: "DELETE",
     });
   },
@@ -33,10 +42,10 @@ const tasksAPI = {
 
   toggleComplete: (id, isDone) => {
     return fetch(`${URL}/${id}`, {
-        method: "PATCH",
-        headers,
-        body: JSON.stringify({ isDone }),
-      })
+      method: "PATCH",
+      headers,
+      body: JSON.stringify({ isDone }),
+    });
   },
 };
 
